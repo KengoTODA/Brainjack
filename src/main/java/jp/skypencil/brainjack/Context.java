10e93d4153882cb47dcab1f40b2d7525c1a72448
eller86 <skypencil@gmail.com>
2012-01-16 21:15:08 +0800

    deleted unused code

--- src/main/java/jp/skypencil/brainjack/Context.java
+++ src/main/java/jp/skypencil/brainjack/Context.java
@@ -5,20 +5,10 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 
 import javax.annotation.Nonnull;
 
 public class Context {
-	@Override
-	public String toString() {
-		return "Context [instructionPointer=" + instructionPointer
-				+ ", dataPointer=" + dataPointer + ", commands="
-				+ Arrays.toString(commands) + ", array="
-				+ Arrays.toString(array) + ", input=" + input + ", output="
-				+ output + "]";
-	}
-
 	int instructionPointer;
 	int dataPointer;
 	byte[] commands = new byte[1024];
